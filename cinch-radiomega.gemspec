7631fc13b2e477c697b27f6727c7372ba9d1120f
Brian Haberer <bhaberer@gmail.com>
2014-01-29 14:21:36 -0800

    Initial version

--- cinch-radiomega.gemspec
+++ cinch-radiomega.gemspec
@@ -10,14 +10,17 @@ Gem::Specification.new do |spec|
   spec.email         = ["bhaberer@gmail.com"]
   spec.description   = %q{Write a gem description}
   spec.summary       = %q{Write a gem summary}
-  spec.homepage      = "n/a"
-  spec.license       = "MIT"
+  spec.homepage      = 'http://github.com/bhaberer/cinch-radiomega/'
+  spec.license       = 'MIT'
 
   spec.files         = `git ls-files`.split($/)
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
-  spec.add_development_dependency "bundler", "~> 1.3"
-  spec.add_development_dependency "rake"
+  spec.add_development_dependency 'bundler', '~> 1.3'
+  spec.add_development_dependency 'rake'
+
+  spec.add_dependency 'jist'
+  spec.add_dependency 'cinch-toolbox'
 end
--- lib/cinch/plugins/radiomega.rb
+++ lib/cinch/plugins/radiomega.rb
@@ -1,15 +1,62 @@
 # -*- coding: utf-8 -*-
 require 'cinch'
+require 'cinch/toolbox'
+require 'jist'
 
 module Cinch::Plugins
   class Radiomega
-    listen_to :channel
+    include Cinch::Plugin
 
     match /(.+) - (.+)/
     set :prefix, /\A\+\+/
 
+    def initialize(*args)
+      super
+      @gist = { id:   config[:gist_id],
+                user: config[:gist_user] }
+    end
+
     def execute(m, title, artist)
-      m.reply 'You linked ' + artist + ' - ' + title
+      current = get_today_gist
+      current << build_song_string(m, title, artist)
+
+      Jist.gist(current.join("\n"),
+                filename: "#{date_text}.txt",
+                update:   @gist[:id])
+    end
+
+    private
+
+    def build_song_string(m, title, artist)
+      nick = m.user.nick
+      time = m.time.strftime("%R")
+      "#{time} < #{nick}> ++ #{title} - #{artist}"
+    end
+
+    def get_today_gist
+      debug gist_url
+      # Get the gist contents for today's list
+      raw_url = Cinch::Toolbox.get_html_element(gist_url,
+                                                "##{gist_file_name} a.raw-url",
+                                                :css_full)
+      raw_url = 'https://gist.github.com' + raw_url[/href=\"(.+)"\s/, 1]
+      debug raw_url
+      list = open(raw_url)
+
+      # Convert to an Array
+      list.read.split("\n")
+    end
+
+    def gist_url
+      "https://gist.github.com/#{@gist[:user]}/#{@gist[:id]}"
+    end
+
+    def gist_file_name(date = date_text)
+      "file-#{date.gsub(/\./, '-')}-txt"
+    end
+
+    def date_text
+      Time.now.strftime('%Y.%m.%d')
     end
   end
 end

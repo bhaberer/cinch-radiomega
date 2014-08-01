d31b9cc206aefbfcef00333f659d7393fcc31060
Brian Haberer <bhaberer@gmail.com>
2014-08-01 10:06:43 -0700

    spec reformatting

--- cinch-radiomega.gemspec
+++ cinch-radiomega.gemspec
@@ -17,14 +17,12 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
-
-  spec.add_development_dependency 'bundler',          '~> 1.3'
-  spec.add_development_dependency 'rake'
-  spec.add_development_dependency  'rspec'
-  spec.add_development_dependency  'coveralls'
-  spec.add_development_dependency  'cinch-test'
-
-  spec.add_dependency             'jist',             '~> 1.5.1'
-  spec.add_dependency             'cinch',            '~> 2.0.12'
-  spec.add_dependency             'cinch-toolbox',    '~> 1.1.1'
+  spec.add_development_dependency 'bundler', '~> 1.3'
+  spec.add_development_dependency 'rake', '~> 10'
+  spec.add_development_dependency 'rspec', '~> 3'
+  spec.add_development_dependency 'coveralls', '~> 0.7'
+  spec.add_development_dependency 'cinch-test', '~> 0.1', '>= 0.1.0'
+  spec.add_dependency 'cinch', '~> 2'
+  spec.add_dependency 'cinch-toolbox', '~> 1.1'
+  spec.add_dependency 'jist', '~> 1.5'
 end
--- lib/cinch/plugins/radiomega.rb
+++ lib/cinch/plugins/radiomega.rb
@@ -1,54 +1,54 @@
 # -*- coding: utf-8 -*-
 require 'cinch'
 require 'cinch/toolbox'
-require 'jist'
 require 'net/http'
 require 'uri'
 
-module Cinch::Plugins
-  # Cinch plugin to gist songs
-  class Radiomega
-    include Cinch::Plugin
+module Cinch
+  module Plugins
+    # Cinch plugin to gist songs
+    class Radiomega
+      include Cinch::Plugin
 
-    match /(.+) - (.+)/,  prefix: /\A\+\+\s/,
-                          method: :log_song
-    match /setlist/,      method: :setlist
+      match(/(.+) - (.+)/, prefix: /\A\+\+\s/, method: :log_song)
+      match(/setlist/, method: :setlist)
 
-    def initialize(*args)
-      super
-      @play_url = config[:play_url]
-    end
+      def initialize(*args)
+        super
+        @play_url = config[:play_url]
+      end
 
-    def setlist(m)
-      m.user.notice 'The setlist for today is at ' +
-                    'http://radiomega.herokuapp.com/setlists/today'
-    end
+      def setlist(m)
+        m.user.notice 'The setlist for today is at ' \
+                      'http://radiomega.herokuapp.com/setlists/today'
+      end
 
-    def log_song(m, title, artist)
-      submit_play_info(artist, title, m.user.nick)
-    end
+      def log_song(m, title, artist)
+        submit_play_info(artist, title, m.user.nick)
+      end
 
-    private
+      private
 
-    def submit_play_info(artist, title, nick)
-      uri = URI.parse(@play_url)
-      http = Net::HTTP.new(uri.host, uri.port)
+      def submit_play_info(artist, title, nick)
+        uri = URI.parse(@play_url)
+        http = Net::HTTP.new(uri.host, uri.port)
 
-      request = Net::HTTP::Post.new(uri.request_uri)
-      request.body = JSON.generate(artist: artist, title: title, nick: nick)
-      request['Content-Type'] = 'application/json'
-      response = http.request(request)
-      debug response.body.to_s
-    end
+        request = Net::HTTP::Post.new(uri.request_uri)
+        request.body = JSON.generate(artist: artist, title: title, nick: nick)
+        request['Content-Type'] = 'application/json'
+        response = http.request(request)
+        debug response.body.to_s
+      end
 
-    def build_song_string(m, title, artist)
-      nick = m.user.nick
-      time = m.time.strftime('%R')
-      [time, "<#{nick}>", '++', title, '-', artist].join(' ')
-    end
+      def build_song_string(m, title, artist)
+        nick = m.user.nick
+        time = m.time.strftime('%R')
+        [time, "<#{nick}>", '++', title, '-', artist].join(' ')
+      end
 
-    def date_text
-      Time.now.strftime('%Y.%m.%d')
+      def date_text
+        Time.now.strftime('%Y.%m.%d')
+      end
     end
   end
 end

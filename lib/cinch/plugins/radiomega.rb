21208ce206c79498c15d2998966edfa15f2381bc
Brian Haberer <bhaberer@gmail.com>
2015-04-21 13:30:56 -0700

    v0.0.9 - Support for Scratch

--- lib/cinch/plugins/radiomega.rb
+++ lib/cinch/plugins/radiomega.rb
@@ -3,6 +3,7 @@ require 'cinch'
 require 'cinch/toolbox'
 require 'net/http'
 require 'uri'
+require 'json'
 
 module Cinch
   module Plugins
@@ -12,38 +13,90 @@ module Cinch
 
       match(/(.+) - (.+)/, prefix: /\A\+\+\s/, method: :log_song)
       match(/setlist/, method: :setlist)
+      match(/email (.+)/, method: :email)
 
       def initialize(*args)
         super
-        @play_url = config[:play_url]
+        @host = config[:host]
+        @play_url = "#{@host}/api/play"
+        @nick_url = "#{@host}/api/nicks"
+        @register_url = "#{@host}/api/register"
+        @scratch_url = "#{@host}/api/scratch"
       end
 
       def setlist(m)
-        m.user.notice 'The setlist for today is at ' \
-                      'http://radiomega.herokuapp.com/setlists/today'
+        m.user.notice 'The setlist for today is at '\
+                      "#{@host}/setlists/today"
       end
 
       def log_song(m, title, artist)
-        submit_play_info(artist, title, m.user.nick)
+        if m.channel.nil?
+          if submit_scratch_track(artist, title, m.user.nick)
+            m.reply "Track added to your scratchpad, see it at #{@host}/scratch"
+          else
+            m.reply "You're sending me a play from private, but you've not "\
+                    "told me your email address on the site. Please tell me "\
+                    "your email address with '.email whatever@place.com' so "\
+                    "I can add this to your scratchpad."
+          end
+        else
+          submit_play_info(artist, title, m.user.nick)
+        end
+      end
+
+      def email(m, email)
+        if submit_email(m.user.nick, email)
+          m.reply "You're all set, you can add scratch tracks now!"
+        else
+          m.reply "That email's already been registered, or you don't have "\
+                  "an account on #{@host}."
+        end
       end
 
       private
 
-      def submit_play_info(artist, title, nick)
-        uri = URI.parse(@play_url)
+      def submit_scratch_track(artist, title, nick)
+        if registered_nicks.include?(nick)
+          result = post_info({ artist: artist, title: title, nick: nick }, @scratch_url)
+          debug "#{result.body}"
+          result.code == '200'
+        else
+          false
+        end
+      end
+
+      def post_info(data, url)
+        uri = URI.parse(url)
         http = Net::HTTP.new(uri.host, uri.port)
 
         request = Net::HTTP::Post.new(uri.request_uri)
-        request.body = JSON.generate(artist: artist, title: title, nick: nick)
+        request.body = JSON.generate(data)
         request['Content-Type'] = 'application/json'
         response = http.request(request)
-        debug response.body.to_s
+        response
+      end
+
+      def submit_play_info(artist, title, nick)
+        post_info({ artist: artist, title: title, nick: nick }, @play_url)
+      end
+
+      def submit_email(nick, email)
+        request = post_info({ email: email, nick: nick }, @register_url)
+        request.code == '200'
       end
 
       def build_song_string(m, title, artist)
         nick = m.user.nick
         time = m.time.strftime('%R')
         [time, "<#{nick}>", '++', title, '-', artist].join(' ')
+      end
+
+      def registered_nicks
+        uri = URI.parse(@nick_url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        request = Net::HTTP::Get.new(uri.request_uri)
+        response = http.request(request)
+        JSON.parse(response.body)
       end
 
       def date_text
--- lib/cinch/plugins/radiomega/version.rb
+++ lib/cinch/plugins/radiomega/version.rb
@@ -3,7 +3,7 @@ module Cinch
   module Plugins
     # Versioning info
     class Radiomega
-      VERSION = '0.0.8'
+      VERSION = '0.0.9'
     end
   end
 end
